413958086dc417daf1da5bc17f5ca3f931d4c760
Saku Etanainen <isilando@shadowblink>
2008-04-22 21:19:40 +0300

    Added DeserialisationException

---
+++ isi/FreeChat/Protocol/MessageCentre/deserialisation_exception.rb
@@ -0,0 +1,10 @@
+module Isi
+  module FreeChat
+    module Protocol
+      module MessageCentre
+        class DeserialisationException < MessageCentreException
+        end
+      end
+    end
+  end
+end
