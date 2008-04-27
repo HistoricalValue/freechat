5b57dc24c9800a350fa3dd813fac4f164d035f99
Saku Etanainen <isilando@shadowblink>
2008-04-27 12:05:29 +0300

    Finished FreeChatUI baseclass

--- isi/FreeChat/free_chat_u_i.rb
+++ isi/FreeChat/free_chat_u_i.rb
@@ -5,8 +5,58 @@ module Isi
     # 
     # All the methods in this class simply print everything to STDOUT.
     # Essentially, all methods should be overriden.
+    #
+    # Each method is specified according to the component a message can arrive
+    # from. The methods accept two arguments, one is the level of the message
+    # (how serious it is or what type of message it is) and the other is
+    # the message itself.
+    # 
+    # The _level_ argument will be one of the constants defined in this class
+    # but no check is performed in the default methods about the type of the
+    # level.
+    #
+    # There is also another generic method, called "generic message" which is
+    # intented for messages not arriving from the predefined componenets. It
+    # gets an extra argument which is the origin of the message.
     class FreeChatUI
+      FATAL  = 0x00
+      ERROR  = 0x01
+      WARNING= 0x04
+      INFO   = 0x08
+      DEBUG  = 0x10
+      FINE   = 0x20
+      FINER  = 0x40
+      FINEST = 0x80
       
+      def generic_message from, level, msg
+        default_print_message_from '?_' + from, level, msg
+      end
+      alias_method :g, :generic_message
+      
+      def linker_message level, msg
+        default_print_message_from 'Linker', level, msg
+      end
+      alias_method :l, :linker_message
+      
+      def post_office_message level, msg
+        default_print_message_from 'PostOffice', level, msg
+      end
+      alias_method :po, :post_office_message
+      
+      def message_centre_message level, msg
+        default_print_message_from 'MessageCentre', level, msg
+      end
+      alias_method :mc, :message_centre_message
+      
+      def bitch_message level, msg
+        default_print_message_from 'Bitch', level, msg
+      end
+      alias_method :b, :bitch_message
+      
+      # This is the method that is called by all default message methods.
+      def default_print_message_from from, level, msg
+        puts "<#{from}:#{level}> #{msg}"
+      end
     end
   end
 end
