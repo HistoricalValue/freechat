b7da019fefb2e827177f9a908d3fb91d8570cbcc
Saku Etanainen <isilando@shadowblink>
2008-04-27 14:44:32 +0300

    Added Linker#get_address_for / Implemented Linker#buddy_using_address

--- isi/FreeChat/Protocol/Linker/linker.rb
+++ isi/FreeChat/Protocol/Linker/linker.rb
@@ -21,14 +21,46 @@ module Isi
             @mediums = {}
             # Special addresses: Maps bid to address which should be used
             # instead of the ones in address book
-            @addr = {}
+            @special_addresses = {}
           end
           
           # Means that the given buddy has initiated a connection from the
           # given address, and therefore this address can be used for sending
           # messages to/through that buddy.
           def buddy_using_address bid, addr
-            
+            addrs = @special_addresses[bid]
+            addrs = [] unless addrs
+            addrs.unshift addr
+            # assert
+            raise unless addr.is_a? Isi::FreeChat::PostOffice::Address
+          end
+          
+          # Gets the appropriate address for the bid.
+          # *NOTICE* this is the address of the medium buddy. This is an
+          # appropriate address that buddy _bid_ is actually using.
+          # If the _retry-ies are too many or if there is no address for this
+          # buddy at all, this method returns nil.
+          # 
+          # === Arguments
+          # * bid: the bid of the buddy
+          # * _retry: which retry is this? If an address returned previously
+          # by this method causes problems, there might be another address.
+          # _retry starts from 0 and can be incremented until this method 
+          # returns nil.
+          def get_address_of bid, _retry = 0
+            special = @special_addresses[bid]
+            normal  = @bbq[bid].addresses
+            # if retry is >0 it means some address is faulty, we should mark
+            # that.
+            # For special addresses, just remove them. For address book addresses
+            # rearrage them
+            if _retry > 0 then
+              case
+              when special then special.shift
+              when normal then normal.push normal.shift
+              end
+            end
+            return special ? special.first : normal ? normal.first : nil
           end
           
           private ##############################################################
