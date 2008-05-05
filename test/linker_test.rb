61b3eb73bcfa9e0684b69b0ca3cc0eaf38bf219c
Saku Etanainen <isilando@shadowblink>
2008-05-05 09:32:26 +0300

    Improving LinkTest: Added BIDs as class constants / created a scenario / tests in progress

--- test/linker_test.rb
+++ test/linker_test.rb
@@ -17,17 +17,28 @@ module Isi
     module Protocol
       module Linker
         class LinkerTest < Test::Unit::TestCase
+          
+          # Bids
+          Kostas = 'Kostas'
+          Marika = 'Marika'
+          Steve  = 'Steve'
+          Aundrey= 'Aundrey'
+          Miranda= 'Miranda'
+          Chandra= 'Chandra'
+          Pekka  = 'Pekka'
+          BIDs = [Kostas, Marika, Steve, Aundrey, Miranda, Chandra, Pekka, ]
+          
           def setup
             @bbq = BuddyBook::BuddyBook.new
             @bens = {
-              'Kostas'  => [['66.66.66.66' , 6666 ], ['68.67.66.65'    , 6463]],
-              'Marika'  => [['87.53.64.21' , 12000], ['162.11.54.69'   , 6969]],
-              'Steve'   => [['54.12.33.21' , 300  ], ['33.67.103.100'  , 300 ],
-                            ['23.67.32.55' , 300  ], ['125.124.122.100', 300 ]],
-              'Aundrey' => [['21.133.87.59', 54281], ],
-              'Miranda' => [['10.10.10.10' , 45   ], ],
-              'Chandra' => [['20.30.40.120', 10   ], ],
-              'Pekka'   => [['23.11.45.65' , 2000 ], ['23.11.45.123'   , 4312]],
+              Kostas  => [['66.66.66.66' , 6666 ], ['68.67.66.65'    , 6463]],
+              Marika  => [['87.53.64.21' , 12000], ['162.11.54.69'   , 6969]],
+              Steve   => [['54.12.33.21' , 300  ], ['33.67.103.100'  , 300 ],
+                          ['23.67.32.55' , 300  ], ['125.124.122.100', 300 ]],
+              Aundrey => [['21.133.87.59', 54281], ],
+              Miranda => [['10.10.10.10' , 45   ], ],
+              Chandra => [['20.30.40.120', 10   ], ],
+              Pekka   => [['23.11.45.65' , 2000 ], ['23.11.45.123'   , 4312]],
             }
             @ui = FreeChatUI.new
             for name, addresses in @bens do
@@ -37,6 +48,28 @@ module Isi
             @link = Linker.new @bbq, @ui
           end
   
+          def test_scenario1
+            # Buddy graph:
+            #
+            #          ,--- Marika    Chandra
+            # Steve --<                 |
+            #   |      `------------ Aundrey --- Kostas
+            #   |                       |
+            #  Miranda                Pekka
+            #
+            # "me" connects directly to Aundrey
+            @link.buddy_connectable Aundrey ; assertions_after_Aundrey
+          end
+          
+          private
+          def assertions_after_Aundrey
+            for bid in BIDs do 
+              assert(!@link.buddy_present?(bid), bid + ' not supposed to be ' +
+                  'known as present yet, but is marked so') \
+                  unless bid == Aundrey
+            end
+          end
+          
         end
       end
     end
