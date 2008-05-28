5e9f9ff42d8cf16fcdf5c37880864f69d3ec09dc
Saku Etanainen <isilando@shadowblink>
2008-05-28 13:45:53 +0300

    Added Isi::db_hello and Isi::db_bye

--- isi/FreeChat/Protocol/Bitch/finder.rb
+++ isi/FreeChat/Protocol/Bitch/finder.rb
@@ -2,12 +2,16 @@ module Isi
   module FreeChat
     module Protocol
       module Bitch
+        Isi::db_hello __FILE__, name
+        
         # Implements the buddy discovery facility of Bitch
         class Finder
           def initialize
             
           end
         end
+        
+        Isi::db_bye __FILE__, name
       end
     end
   end
