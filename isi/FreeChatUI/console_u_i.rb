c5ae41174a3f29a7f8afea71ea26d4d8c8db8cce
Saku Etanainen <isilando@shadowblink>
2008-06-02 12:50:04 +0300

    Added @cursor , read_lines , unread? , unread , length and fixed << to update the @cursor too.

--- isi/FreeChatUI/console_u_i.rb
+++ isi/FreeChatUI/console_u_i.rb
@@ -23,6 +23,7 @@ module Isi
           @lines = []
           @title = title
           @buf_size = buf_size
+          @cursor = 0 # points to the first unread line
         end
         attr_reader :id, :title
         attr_accessor :buf_size
@@ -35,7 +36,11 @@ module Isi
         def << line
           if line.is_a?(String) then
             @lines << line
-            while @lines.length >= @buf_size do @lines.shift end
+            while length >= @buf_size do
+              @lines.shift
+              @cursor -= 1
+            end
+            @cursor = 0 if @cursor < 0
             true
           else
             false
@@ -43,12 +48,51 @@ module Isi
         end
         alias_method :append_line, :'<<'
         
+        def length
+          @lines.length
+        end
+        
+        # Returns true if there are unread lines in this window
+        def unread?
+          length > @cursor
+        end
+        # Return the number of unrea lines
+        def unread
+          length - @cursor
+        end
+        
+        # Will invoke +[]+ on the underlying array of lines. Nothing is marked
+        # as read.
         def [](*args, &block)
           @lines.[](*args, &block)
         end
         
-        def length
-          @lines.length
+        #     read_lines(n) -> [line(k), line(k+1), ... , line(k+n-1)]
+        # Returns an array of the newer _n_ lines and marks them as read.
+        # If _n_ is nil then all unread lines are returned and marked as read.
+        # If _n_ is greater than the number of unread lines currently, then
+        # some the extra lines are taken from the older lines, right before
+        # the new, unread ones. All the new lines will be marked as read.
+        # If _n_ is greater than +buf_size+ then an +ArgumentError+ is raised,
+        # just for fun.
+        # If optional argument _bigmac_ does not evaluate to _false_, then
+        # +read_lines+ will return (and mark as read accordingly) whichever
+        # is more: the actual unread lines or _n_ lines.
+        def read_lines n=nil, bigmac=false
+          unlines = unread
+          if n.nil? || bigmac && unlines > n then n = unlines end
+          raise ArgumentError::new("n>buf_size (#{n}>#{buf_size}). Told you") \
+              if n > buf_size
+          result = []
+          if n > unlines then
+            padding = n - unlines
+            n = unlines
+            result.concat(self[@cursor - padding .. @cursor - 1])
+          end
+          new_cursor = @cursor + n
+          result.concat(self[@cursor .. new_cursor])
+          @cursor = new_cursor
+          result
         end
         
         # instances accessible only through factory methods
@@ -57,6 +101,8 @@ module Isi
           @@last_id += 1
           new(@@last_id, title)
         end
+        
+        private ################################################################
       end
       
       @@DefaultCommandHandlerClass = Class::new {
